35f7a592afec954b624b2dea315383c336c93d32
Chu-Nyan <141575778+Chu-Nyan@users.noreply.github.com>
2023-09-26 15:57:27 +0900

    빌딩 스크립트 추가

---
+++ Assets/Scripts/ForgeBuilding.cs
@@ -0,0 +1,15 @@
+﻿internal class ForgeBuilding : BaseBuilding
+{
+
+    protected override void Awake()
+    {
+        base.Awake();
+        buildingType = BuildingType.Inn;
+    }
+
+    public void UpgradeUnit()
+    {
+        // 유닛 업그레이드
+    }
+}
+
