054f48755144f7ea2d926d12df4d11bcc919af79
Chu-Nyan <141575778+Chu-Nyan@users.noreply.github.com>
2023-09-27 14:48:30 +0900

    클릭빌딩 UI 애니 추가

--- Assets/Scripts/Entities/UI/ClickBuildingUI.cs
+++ Assets/Scripts/Entities/UI/ClickBuildingUI.cs
@@ -6,29 +6,32 @@ using UnityEngine;
 
 public class ClickBuildingUI : MonoBehaviour
 {
-    [SerializeField] TMP_Text lvNameText;
-    [SerializeField] TMP_Text currentEffectText;
-    [SerializeField] TMP_Text upgradeText;
-    private StringBuilder newStatusText = new();
+    enum ClickBuildUIText { Name, CurrentEffect, Upgrade }
 
+    [SerializeField] TMP_Text[] texts;
+    private StringBuilder newStatusText = new();
 
     public void On(BaseBuilding building)
     {
         Vector3 screenPos = Camera.main.WorldToScreenPoint(building.transform.position);
         transform.position = screenPos;
-        gameObject.SetActive(true);
+
+        if (!gameObject.activeSelf)
+            gameObject.SetActive(true);
     }
 
     public void OFF()
     {
+        ActiveRaycastTargrt(false);
         gameObject.SetActive(false);
     }
 
     public void Refresh(BaseBuilding target)
     {
+
         newStatusText.Clear();
         newStatusText.Append($"Lv. {target.level} {target.buildingName}");
-        lvNameText.text = newStatusText.ToString();
+        texts[(int)ClickBuildUIText.Name].text = newStatusText.ToString();
 
         newStatusText.Clear();
         switch (target.buildingType)
@@ -42,10 +45,20 @@ public class ClickBuildingUI : MonoBehaviour
             default:
                 break;
         }
-        currentEffectText.text = newStatusText.ToString();
+        texts[(int)ClickBuildUIText.CurrentEffect].text = newStatusText.ToString();
 
         newStatusText.Clear();
         newStatusText.Append($"{target.upgradeWood}");
-        upgradeText.text = newStatusText.ToString();
+        texts[(int)ClickBuildUIText.Upgrade].text = newStatusText.ToString();
     }
+    // 아이콘 레이캐스트 스위치
+    public void ActiveRaycastTargrt(bool isON)
+    {
+        foreach (var item in texts)
+        {
+            item.raycastTarget = true;
+        }
+    }
+
+
 }
