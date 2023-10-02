6e338172561bbf2eafc738800071b2a4b8d5ac3b
DoDokang318 <110431311+DoDokang318@users.noreply.github.com>
2023-10-02 20:09:17 +0900

    [install]2d
    2d패키지 설치

--- Assets/Scripts/Controllers/MercenaryController.cs
+++ Assets/Scripts/Controllers/MercenaryController.cs
@@ -2,7 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
-//using Unity.VisualScripting;
+
+using UnityEditor;
+
 using UnityEditor;
 
 using UnityEngine;
