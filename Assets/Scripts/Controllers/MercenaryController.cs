347f55e73ae1296dcfbd46f2ea760f6b48391a72
DoDokang318 <110431311+DoDokang318@users.noreply.github.com>
2023-10-02 20:02:13 +0900

    dd
    dd

--- Assets/Scripts/Controllers/MercenaryController.cs
+++ Assets/Scripts/Controllers/MercenaryController.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
-using Unity.VisualScripting;
-using UnityEditor;
 //using Unity.VisualScripting;
+using UnityEditor;
+
 using UnityEngine;
 using UnityEngine.PlayerLoop;
 
