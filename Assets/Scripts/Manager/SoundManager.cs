afc431c54d18636760af323ec4af91db9628e514
DoDokang318 <110431311+DoDokang318@users.noreply.github.com>
2023-10-03 12:36:51 +0900

    [feat]사운드 관련기능추기
    1 볼륨컨트롤
    2 씬마다 배경음악 다르게덕용
    3 사운드매니저

--- Assets/Scripts/Manager/SoundManager.cs
+++ Assets/Scripts/Manager/SoundManager.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
 
 
 public class SoundManager : MonoBehaviour
@@ -9,8 +12,8 @@ public class SoundManager : MonoBehaviour
 
     public AudioSource BgmAudioSource;
 
-    public AudioClip BgmAudioClip;
-
+    public AudioClip[] BgmAudioClips;
+    enum BgmScene { StartScene, MainScene }
 
     void Awake()
     {
@@ -24,6 +27,8 @@ public class SoundManager : MonoBehaviour
             Destroy(this.gameObject);
         }
     }
+
+ 
     public static SoundManager Instance
     {
         get
@@ -35,13 +40,40 @@ public class SoundManager : MonoBehaviour
             return instance;
         }
     }
-
-
-
-    public void PlayBgmSound()
+    private void OnEnable()
     {
-        BgmAudioSource.clip = BgmAudioClip;
-        BgmAudioSource.Play();
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        // 씬 이름을 기반으로 해당 씬에 맞는 배경 음악을 선택하여 재생
+        string sceneName = scene.name;
+        AudioClip selectedClip = null;
+
+        // 씬 이름에 따라 다른 배경 음악을 선택
+        if (sceneName == "StartScene")
+        {
+            selectedClip = BgmAudioClips[(int)BgmScene.StartScene];
+        }
+        else if (sceneName == "MainScene")
+        {
+            selectedClip = BgmAudioClips[(int)BgmScene.MainScene];
+        }
+        // 다른 씬에 대한 처리도 추가
+
+        // 선택된 배경 음악을 재생
+        if (selectedClip != null)
+        {
+            BgmAudioSource.clip = selectedClip;
+            BgmAudioSource.Play();
+        }
     }
 
 
