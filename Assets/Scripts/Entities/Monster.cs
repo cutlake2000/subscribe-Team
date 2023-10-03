8ccfdce67b5a07318b1413d40a3085b12a6a0a5e
DoDokang318 <110431311+DoDokang318@users.noreply.github.com>
2023-10-03 16:58:05 +0900

    [bug] 몬스터 스폰관련 버그 수정
    - 낮에도 카운트 되는 몬스터 수정
    - 그라운드위 ui추가
    - 몬스터 죽을시 자원 추가
    - 낮밤 바뀔때 필드위 몬스터 전부 파괴

--- Assets/Scripts/Entities/Monster.cs
+++ Assets/Scripts/Entities/Monster.cs
@@ -18,11 +18,24 @@ public class Monster : MonoBehaviour
         if (MonsterHp <= 0)
             Die();
     }
+    void Update()
+    {
+        // 스페이스바를 누를 때 Die() 메서드를 호출합니다.
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Die();
+        }
+    }
 
     void Die()
     {
-        
-
+        float randomValue = Random.value;    
+        if (randomValue <= 0.5f)
+        {  
+            DataManager.Instance.player.Gold++;
+            DataManager.Instance.player.Wood++;
+            Debug.Log("자원+");
+        }      
         Destroy(gameObject);
     }
 }
